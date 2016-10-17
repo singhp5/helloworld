cfb45f5a164b698aff4ba225d5be0bb8bf490b68
Prabhjot Singh <singhp5@uw.edu>
2016-10-17 09:55:51 -0700

    done

--- src/helloNewWorld/hello.java
+++ src/helloNewWorld/hello.java
@@ -1,5 +1,13 @@
 package helloNewWorld;
 
+
 public class hello {
 
+	public static void main(String[] args) {
+		System.out.println("Working on git");
+		System.out.println("new to the git");
+
+	}
+
 }
+
